7ed670fad9a3b9a0db781836d53bce6adf10c844
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-01 18:38:06 +0600

    Explored routing and nested routing

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="font-bold text-center text-5xl">This is login page</h2>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/registration/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="font-bold text-center text-5xl">
+        This is registration page
+      </h2>
+    </div>
+  );
+};
+
+export default page;
