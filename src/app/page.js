5fb1cbd2f8700c3141f602248793231aa55fddef
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-01 15:47:25 +0600

    NextJS environment setup completed

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="container mx-auto">
+      <h1 className="text-center text-5xl font-bold">Hablu Programmers</h1>
+    </main>
+  );
+}
