e8a281347e5540c1e655cc54c48b362e5889192b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-03 19:32:27 +0600

    Explored group and conditional layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,46 +18,52 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div className="container mx-auto mt-4">
-          <ul className="text-xl mb-10 text-green-500 flex justify-center gap-8">
-            <li>
-              <Link
-                href={"/"}
-                className={`${
-                  pathName === "/"
-                    ? "bg-black text-white p-2 rounded-xl font-bold"
-                    : ""
-                }`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathName === "/about"
-                    ? "bg-black text-white p-2 rounded-xl font-bold"
-                    : ""
-                }`}
-                href={"/about"}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={`${
-                  pathName === "/userList"
-                    ? "bg-black text-white p-2 rounded-xl font-bold"
-                    : ""
-                }`}
-                href={"/userList"}
-              >
-                User List
-              </Link>
-            </li>
-            <li>Contact</li>
-            <li>SignUp</li>
-          </ul>
+          {pathName !== "/userList" ? (
+            <ul className="text-xl mb-10 text-green-500 flex justify-center gap-8">
+              <li>
+                <Link
+                  href={"/"}
+                  className={`${
+                    pathName === "/"
+                      ? "bg-black text-white p-2 rounded-xl font-bold"
+                      : ""
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`${
+                    pathName === "/about"
+                      ? "bg-black text-white p-2 rounded-xl font-bold"
+                      : ""
+                  }`}
+                  href={"/about"}
+                >
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`${
+                    pathName === "/userList"
+                      ? "bg-black text-white p-2 rounded-xl font-bold"
+                      : ""
+                  }`}
+                  href={"/userList"}
+                >
+                  User List
+                </Link>
+              </li>
+              <li>Contact</li>
+              <li>SignUp</li>
+            </ul>
+          ) : (
+            <Link href={"/"} className="flex justify-center font-bold">
+              Home
+            </Link>
+          )}
           {children}
         </div>
       </body>
