1a5317af5c917392fc525033fb3bb24ab771f255
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-03 16:36:48 +0600

    Explored file colocation and private folder

---
+++ src/app/_hablu/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="min-h-screen flex justify-center items-center font-bold text-5xl">
+        Private Folder
+      </h2>
+    </div>
+  );
+};
+
+export default page;
