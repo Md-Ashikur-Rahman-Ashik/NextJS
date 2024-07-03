7bf9fdafae6a91f698eea848bd560001a75fd42c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-03 16:21:09 +0600

    Created 404 page route

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const notFound = () => {
+  return (
+    <div className="container mx-auto">
+      <h2 className="min-h-screen flex justify-center items-center font-bold text-red-500 text-5xl">
+        Our custom made 404 page is working
+      </h2>
+    </div>
+  );
+};
+
+export default notFound;
