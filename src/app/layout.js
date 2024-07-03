5ac20113362c16113e39fd98825aeabd0be4bb14
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-03 18:37:15 +0600

    Explored active links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,59 @@
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Welcome hablu uncle",
-  description: "As Salamu Alaikum, Uncle",
-};
+// export const metadata = {
+//   title: "Welcome hablu uncle",
+//   description: "As Salamu Alaikum, Uncle",
+// };
 
 export default function RootLayout({ children }) {
+  const pathName = usePathname();
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="container mx-auto">
+        <div className="container mx-auto mt-4">
           <ul className="text-xl mb-10 text-green-500 flex justify-center gap-8">
             <li>
-              <Link href={"/"}>Home</Link>
+              <Link
+                href={"/"}
+                className={`${
+                  pathName === "/"
+                    ? "bg-black text-white p-2 rounded-xl font-bold"
+                    : ""
+                }`}
+              >
+                Home
+              </Link>
             </li>
             <li>
-              <Link href={"/about"}>About</Link>
+              <Link
+                className={`${
+                  pathName === "/about"
+                    ? "bg-black text-white p-2 rounded-xl font-bold"
+                    : ""
+                }`}
+                href={"/about"}
+              >
+                About
+              </Link>
             </li>
             <li>
-              <Link href={"/userList"}>User List</Link>
+              <Link
+                className={`${
+                  pathName === "/userList"
+                    ? "bg-black text-white p-2 rounded-xl font-bold"
+                    : ""
+                }`}
+                href={"/userList"}
+              >
+                User List
+              </Link>
             </li>
             <li>Contact</li>
             <li>SignUp</li>
